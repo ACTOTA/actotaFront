fd2d53ad2ee2f79320ff0f00248f50105b597315
tyler@adventurecoloradotours.com <tylerdevries22@gmail.com>
2024-04-27 13:54:01 -0600

    loginand out

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -6,8 +6,16 @@ import { useState, useCallback} from "react";
 import MenuItem from "./MenuItem";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
+import { User } from "@prisma/client";
+import { signOut } from "next-auth/react";
 
-const UserMenu = () => {
+interface UserMenuProps {
+    currentUser?: User | null;
+    }
+
+const UserMenu: React.FC<UserMenuProps> = (
+    currentUser
+) => {
     const registerModal = useRegisterModal();
     const loginModal = useLoginModal();
     const [isOpen, setIsOpen] = useState(false);
@@ -79,6 +87,33 @@ const UserMenu = () => {
                     "
                 >
                     <div className="flex flex-col cursor-pointer">
+                        
+                        {currentUser ? (
+                        <>
+                            <MenuItem 
+                                onClick={() => {}}
+                                label="My Trips"
+                            />
+                             <MenuItem 
+                                onClick={() => {}}
+                                label="My Favorites"
+                            />
+                            <MenuItem 
+                                onClick={() => {}}
+                                label="My Reservations"
+                            />
+                            <hr />
+                            <MenuItem 
+                                onClick={loginModal.onOpen}
+                                label="Log In"
+                            />
+                            <MenuItem 
+                                onClick={() => signOut()}
+                                label="Logout"
+                            />
+
+                        </>
+                        ) : (
                         <>
                             <MenuItem 
                                 onClick={loginModal.onOpen}
@@ -89,6 +124,7 @@ const UserMenu = () => {
                                 label="Sign Up"
                             />
                         </>
+                        )}
                     </div>
                 </div>
             )}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+//by default a server component 
+
 import React from "react";
 import { Nunito } from 'next/font/google';
 
@@ -7,6 +9,7 @@ import ClientOnly from './components/ClientOnly';
 import RegisterModal from './components/modals/RegisterModal';
 import ToasterProvider from './providers/ToasterProvider';
 import LoginModal from "./components/modals/LoginModal";
+import getCurrentUser from "./actions/getCurrentUser";
 
 export const metadata = {
   title: "ACTOTA!",
@@ -17,11 +20,12 @@ const font = Nunito({
   subsets: ['latin'], 
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
+  const currentUser = await getCurrentUser();
   return (
     <html lang="en">
       <body className={font.className}>
@@ -29,7 +33,7 @@ export default function RootLayout({
           <ToasterProvider />
           <LoginModal />
           <RegisterModal />
-          <Navbar />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}
       </body>
