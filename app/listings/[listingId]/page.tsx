540197803afcbfd7aae1cdebf7977b3b013d8072
tyler@adventurecoloradotours.com <tylerdevries22@gmail.com>
2024-05-04 12:43:29 -0600

    listing page partial

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,50 +1,39 @@
 
-const ListingPage = () => {
-  return (
-    <div>
-      my listing page
-    </div>
-  );
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import getListingById from "@/app/actions/getListingById";
+import getReservations from "@/app/actions/getReservations";
+
+import ClientOnly from "@/app/components/ClientOnly";
+import EmptyState from "@/app/components/EmptyState";
+
+import ListingClient from "./ListingClient";
+
+interface IParams {
+  listingId?: string;
 }
 
-export default ListingPage;
+const ListingPage = async ({ params }: { params: IParams }) => {
 
-// import getCurrentUser from "@/app/actions/getCurrentUser";
-// // import getListingById from "@/app/actions/getListingById";
-// // import getReservations from "@/app/actions/getReservations";
+  const listing = await getListingById(params);
+  const reservations = await getReservations(params);
+  const currentUser = await getCurrentUser();
 
-// import ClientOnly from "@/app/components/ClientOnly";
-// import EmptyState from "@/app/components/EmptyState";
+  if (!listing) {
+    return (
+      <ClientOnly>
+        <EmptyState />
+      </ClientOnly>
+    );
+  }
 
-// // import ListingClient from "./ListingClient";
-
-// interface IParams {
-//   listingId?: string;
-// }
-
-// const ListingPage = async ({ params }: { params: IParams }) => {
-
-//   // const listing = await getListingById(params);
-//   // const reservations = await getReservations(params);
-//   const currentUser = await getCurrentUser();
-
-//   // if (!listing) {
-//     return (
-//       <ClientOnly>
-//         <EmptyState />
-//       </ClientOnly>
-//     );
-  
-
-//   // return (
-//   //   <ClientOnly>
-//   //     {/* <ListingClient
-//   //       listing={listing}
-//   //       // reservations={reservations}
-//   //       currentUser={currentUser}
-//   //     /> */}
-//   //   </ClientOnly>
-//   // );
-// }
+  return (
+    <ClientOnly>
+      <ListingClient
+        listing={listing}
+        currentUser={currentUser}
+      />
+    </ClientOnly>
+  );
+}
  
-// export default ListingPage;
+export default ListingPage;
