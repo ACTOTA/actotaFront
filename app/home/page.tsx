30e17474b00acfc294391ae9ebae2a9fcfa3d141
Andrew Cooksey <andrewcooksey42@gmail.com>
2024-05-11 11:17:42 -0500

    moved video banner

---
+++ app/home/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../../app/components/Banner';
+
+export default function Home() {
+
+
+  return (
+    <>
+      <div>
+        <Banner />
+      </div>
+    </>
+  )
+}
