96f5a83951330e6b3f1875499ec8c401055b23ed
Andrew Cooksey <andrewcooksey42@gmail.com>
2024-07-11 21:29:08 -0500

    app/components/page.ts

---
+++ app/components/page.ts
@@ -0,0 +1,14 @@
+'use client';
+import { useEffect } from "react";
+
+
+const Components = () => {
+
+
+	useEffect(() => {
+		console.log("Hello, World!");
+	}, []);
+
+}
+
+export default Components;
