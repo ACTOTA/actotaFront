a2de01b3a176a99c2a834a59e07f3c954a44932c
Nes1998 <fong.nestor@yahoo.com>
2024-07-21 22:59:48 -0500

    Working on the knob

--- app/components/figma/Knob.tsx
+++ app/components/figma/Knob.tsx
@@ -1,6 +1,15 @@
 export default function Knob({ children}: { children: React.ReactNode}) {
     return (
         
+        <button
+        type="button"
+        className="rounded-[200px] top-[24px] w-[48px] h-[48px] opacity-20 flex items-center justify-center neutral-06"
+      >
+        <button type="button"
+        className="rounded-[200px] h-[24px] w-[24px] top-[36px] left-[12px] neutral-06">
+
+        </button>
+      </button>
     ) 
        
 }
