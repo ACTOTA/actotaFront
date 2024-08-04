67c4f82988d768e931d0073798398eb2df715a6f
Nes1998 <fong.nestor@yahoo.com>
2024-08-04 00:05:25 -0500

    Working on components

---
+++ app/components/figma/Loader.tsx
@@ -0,0 +1,10 @@
+export default function Loader() {
+    return (
+        <div className="flex text-center w-[304px] h-[88px] gap-[8px] translucent-black50">
+            <div className="w-[304px] h-[56px] rounded-[16px] border-[1px] justify-between py-[16px] px-[0px] translucent-black30">
+
+            </div>
+        </div>
+    )
+}
+
