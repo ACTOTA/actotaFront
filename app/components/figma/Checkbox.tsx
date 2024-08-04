3fe7cdb96d4908f43b6f6f1ab14c8b1e5390e749
Nes1998 <fong.nestor@yahoo.com>
2024-08-03 21:42:35 -0500

    Working on the components

--- app/components/figma/Checkbox.tsx
+++ app/components/figma/Checkbox.tsx
@@ -2,8 +2,8 @@ import { MdCheckBox } from "react-icons/md"
 
 export default function Checkbox({ children, checked}: { children: React.ReactNode, checked: boolean}) {
     return (
-        <div className="flex items-center gap-2 neutral-06">
-            <MdCheckBox aria-checked={checked} />
+        <div className="flex text-center gap-2 neutral-06">
+            <MdCheckBox aria-checked={checked} className="justify-start"/>
             <span className="text-neutral-04 width-[212px] height-24px">
                 {children}
             </span>
--- app/components/figma/DropdownMenu.tsx
+++ app/components/figma/DropdownMenu.tsx
@@ -1,8 +1,12 @@
+import { RiMapPinLine } from 'react-icons/ri';
+
 export default function Dropdownmenu() {
     return (
         <div className="flex items-center gap-2">
-            <div className="w-4 h-4 rounded-full bg-gray-300"></div>
-            <span>Dropdownmenu</span>
+            <div className="w-[226px] h-[44px] gap-[12px] rounded-[8px] top-[124px] left-[40px] flex py-[12px] px-[0px]">
+                <RiMapPinLine className='border-[1.25px] border-neutral-06'/>
+                <span className="text-neutral-05 w-[170px] h-[20px] text-center">Dropdown</span>
+            </div>
         </div>
     )
 }
