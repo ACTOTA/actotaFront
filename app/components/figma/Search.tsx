62c192341108c7465ce40514c392b84c71c9017b
Nes1998 <fong.nestor@yahoo.com>
2024-08-05 19:03:16 -0500

    Working on components

--- app/components/figma/Search.tsx
+++ app/components/figma/Search.tsx
@@ -1,13 +1,14 @@
+import { DivIcon } from "leaflet"
 import { BiSearch } from "react-icons/bi"
 
 export default function Search({children}: {children: React.ReactNode}) {
     return (
-        <button className="w-[687px] h-[96px] rounded-[200px] border-[1px] gap-[16px] p-[0px, 16px, 0px, 0px] glass-corner stroke-glass1">
+        <div className="w-[687px] h-[96px] rounded-[200px] border-[1px] gap-[16px] p-[0px, 16px, 0px, 0px] glass-corner stroke-glass1">
             {children}
             
-            <button className="rounded-[32px] gap-[32px] p-[24px] w-[64px] h-[64px] neutral-06 flex items-center">
+            <div className="rounded-[32px] gap-[32px] p-[24px] w-[64px] h-[64px] neutral-06 flex items-center">
                 <BiSearch className="w-[24px] h-[24px] text-neutral-04" />
-            </button>
-        </button>
+            </div>
+        </div>
     )
 }
