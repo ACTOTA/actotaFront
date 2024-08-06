cf5437f1fc23285abc21a08809c8f3be5ee543fe
Nes1998 <fong.nestor@yahoo.com>
2024-08-05 19:04:36 -0500

    Working on components

--- app/components/figma/Search.tsx
+++ app/components/figma/Search.tsx
@@ -3,10 +3,10 @@ import { BiSearch } from "react-icons/bi"
 
 export default function Search({children}: {children: React.ReactNode}) {
     return (
-        <div className="w-[687px] h-[96px] rounded-[200px] border-[1px] gap-[16px] p-[0px, 16px, 0px, 0px] glass-corner stroke-glass1">
+        <div className="w-[687px] h-[96px] rounded-[200px] border-[1px] gap-[16px] p-[0px, 16px, 0px, 0px] glass-corner stroke-glass1 flex items-center">
             {children}
             
-            <div className="rounded-[32px] gap-[32px] p-[24px] w-[64px] h-[64px] neutral-06 flex items-center">
+            <div className="rounded-[32px] gap-[32px] p-[24px] w-[64px] h-[64px] neutral-06 flex justify-start">
                 <BiSearch className="w-[24px] h-[24px] text-neutral-04" />
             </div>
         </div>
