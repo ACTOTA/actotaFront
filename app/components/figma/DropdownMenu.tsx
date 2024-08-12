b577a521251a6b2cb49f5c35ec1f446d0a753dd2
Nes1998 <fong.nestor@yahoo.com>
2024-08-12 18:22:35 -0500

    Working on the components

--- app/components/figma/DropdownMenu.tsx
+++ app/components/figma/DropdownMenu.tsx
@@ -1,5 +1,6 @@
 import { RiMapPinLine } from 'react-icons/ri';
 
+
 export default function Dropdownmenu() {
     return (
         <div className="w-[226px] h-[44px] gap-[12px] rounded-[8px] top-[124px] left-[40px] flex py-[12px] px-[0px] border-[1px] border-neutral-06 translucent-black30">
