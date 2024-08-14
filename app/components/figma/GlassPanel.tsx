7f8b78f67489ecb95c3724a48830a85ef78b7840
Nes1998 <fong.nestor@yahoo.com>
2024-08-13 22:53:50 -0500

    Working on the components

--- app/components/figma/GlassPanel.tsx
+++ app/components/figma/GlassPanel.tsx
@@ -1,3 +1,8 @@
+type GlassPanelProps = {
+    children: React.ReactNode;
+    imageProps?: React.ReactNode;
+} & React.HTMLAttributes<HTMLDivElement>;
+
 export default function GlassPanel() {
     return (
         <div className="w-[584px] h-[824px] rounded-[48px] border-[1px] flex padding-[40px] gap-[32px] glass-corner stroke-glass1" >
