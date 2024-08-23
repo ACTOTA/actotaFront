3499a6da8e2a7bf9ae055e1873e9180c750c2d2d
Nes1998 <fong.nestor@yahoo.com>
2024-08-22 22:14:06 -0500

    Working on the compoents

--- app/components/figma/Chip.tsx
+++ app/components/figma/Chip.tsx
@@ -1,5 +1,10 @@
 import { FaPlusMinus } from "react-icons/fa6";
 
+type chipProps = {
+    children: React.ReactNode;
+    className?: string;
+} & React.HTMLAttributes<HTMLDivElement>;
+
 export default function Chip() {
     return (
         <div className="flex text-center w-[124px] h-[40px] rounded-[200px] border-[1px] py-[12px] px-[8px] gap-[8px] translucent-black50">
