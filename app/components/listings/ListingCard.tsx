3a4a4182c0903df92b7cb89472156705b6d948c5
Andrew Cooksey <andrewcooksey42@gmail.com>
2024-09-14 14:32:41 -0500

    listing card updates

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -73,77 +73,86 @@ const ListingCard: React.FC<ListingCardProps> = ({
 
     const handleCancel = useCallback(
         (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.stopPropagation();
-    
-        if (disabled) {
-          return;
-        }
-    
-        onAction?.(actionId)
-      }, [disabled, onAction, actionId]);
+            e.stopPropagation();
+
+            if (disabled) {
+                return;
+            }
+
+            onAction?.(actionId)
+        }, [disabled, onAction, actionId]);
 
     const price = useMemo(() => reservation?.totalPrice || data.price, [reservation, data.price]);
 
 
-  const reservationDate = useMemo(() => {
-    if (!reservation) {
-      return null;
-    }
-  
-    const start = new Date(reservation.startDate);
-    const end = new Date(reservation.endDate);
+    const reservationDate = useMemo(() => {
+        if (!reservation) {
+            return null;
+        }
 
-    return `${format(start, 'PP')} - ${format(end, 'PP')}`;
-  }, [reservation]);
-  
+        const start = new Date(reservation.startDate);
+        const end = new Date(reservation.endDate);
+
+        return `${format(start, 'PP')} - ${format(end, 'PP')}`;
+    }, [reservation]);
+
     return (
         <div onClick={() => router.push(`/listings/${data.id}`)}
-            className='col-span-1 cusrsor-pointer group'
+            className='col-span-1 cusrsor-pointer group w-full h-[400px]'
         >
-            <div className='flex flex-col w-full gap-2'>
-                <div className='relative w-full overflow-hidden aspect-square rounded-xl'>
+            <div className='flex flex-col w-full h-full gap-2 relative'>
+                <div className='absolute w-full h-full overflow-hidden aspect-square rounded-xl'>
                     <Image
                         fill
                         alt="Listing"
                         src={data.imageSrc}
-                        className='object-cover w-full h-full transition-transform duration-200 ease-in-out group-hover:scale-110'
+                        className='object-cover transition-transform duration-200 ease-in-out group-hover:scale-110'
                     />
-                    <div className='absolute top-3 right-3'>
-                        <StarButton
-                            listingId={data.id}
-                            currentUser={currentUser}
-                        />
-                    </div>
-                </div>
-                
-                <div className="text-lg font-semibold">
-                    {location?.label}, {location?.region}
+
                 </div>
 
-                <div className="font-light text-neutral-500">
-                {reservationDate || data.activity}
-                </div>
-                
-                
-                <div className="flex flex-row items-center gap-1">
-                <div className="font-semibold">
-                    $ {price}
-                </div>
-                {!reservation && (
-                    <div className="font-light">night</div>
-                )}
-                </div>
-                
-            {onAction && actionLabel && (
-                <Button
-                    disabled={disabled}
-                    small
-                    label={actionLabel} 
-                    onClick={handleCancel}
-                />
-                )}
+                <section className='absolute h-full w-full p-2'>
+                    <div className='h-1/5'>
+                        <StarButton
+                            listingId={data.id}
+                            currentUser={currentUser} />
                     </div>
-                </div>
+
+                    <div className='h-2/5' />
+
+                    <div className="glass-dark glass-blur glass-stroke-1 h-2/5 rounded-[8px] p-2 text-white">
+
+                        <div className="text-lg font-semibold">
+                            {location?.label}, {location?.region}
+                        </div>
+
+                        <div className="font-light text-neutral-500">
+                            {reservationDate || data.activity}
+                        </div>
+
+
+                        <div className="flex flex-row items-center gap-1">
+                            <div className="font-semibold">
+                                $ {price}
+                            </div>
+                            {!reservation && (
+                                <div className="font-light">night</div>
+                            )}
+                        </div>
+
+                    </div>
+                </section>
+
+                {onAction && actionLabel && (
+                    <Button
+                        disabled={disabled}
+                        small
+                        label={actionLabel}
+                        onClick={handleCancel}
+                    />
+                )}
+            </div>
+        </div>
     );
 }
 
